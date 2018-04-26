5fb425c0891cab92d5300381d0ca4fa286a73088
asecino1999 <asecino1999@gmail.com>
2018-04-26 11:23:08 -0500

    nuevo archivo: creaBase.sh

---
+++ creaBase.sh
@@ -0,0 +1,16 @@
+mkdir pagina
+cd pagina
+mkdir image
+mkdir script
+mkdir styles
+touch index.html
+echo "<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+
+  </body>
+</html>" >> index.html
