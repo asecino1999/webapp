af09c7607e456a7a2e8c0f35597ece8e3ee381af
asecino1999 <asecino1999@gmail.com>
2018-04-26 18:49:56 -0500

    modificado:    creaBase.sh

--- creaBase.sh
+++ creaBase.sh
@@ -1,8 +1,14 @@
-mkdir pagina
-cd pagina
+
+mkdir sources
+cd sources
+mkdir js
+mkdir css
 mkdir image
-mkdir script
-mkdir styles
+cd ..
+mkdir vendor
+cd vendor
+wget https://code.jquery.com/jquery-3.3.1.min.map
+cd ..
 touch index.html
 echo "<!DOCTYPE html>
 <html>
