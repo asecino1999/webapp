f339b7d9be5218969b86e2155c3135cd3e66100a
asecino1999 <asecino1999@gmail.com>
2018-05-03 19:00:13 -0500

    modificado:    creaBase.sh

--- creaBase.sh
+++ creaBase.sh
@@ -2,21 +2,33 @@
 mkdir sources
 cd sources
 mkdir js
+cd js
+touch script.js
+cd ..
+
 mkdir css
+cd css
+touch style.css
+cd ..
 mkdir image
 cd ..
 mkdir vendor
 cd vendor
 wget https://code.jquery.com/jquery-3.3.1.min.map
 cd ..
+
+ls
 touch index.html
 echo "<!DOCTYPE html>
 <html>
   <head>
-    <meta charset="utf-8">
+    <meta charset='utf-8'>
+    <link rel='stylesheet' type='text/css' href='sources/css/style.css'>
+    <script src='sources/js/script.js'></script>
     <title></title>
   </head>
   <body>
 
   </body>
+
 </html>" >> index.html
